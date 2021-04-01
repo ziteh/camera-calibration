5dac83e30a0a8f336772cfd1a3d0d035753cd1b5
ziteh <honmonoh@gmail.com>
2021-04-01 17:06:00 +0800

    增加 Drawing 的參考

--- Camera Calibration/CameraCalibration.cs
+++ Camera Calibration/CameraCalibration.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace CameraCalibration
 {
     public class CameraCalibration
     {
+        public PointF ImageToWorld()
+        {
+        }
     }
 }
