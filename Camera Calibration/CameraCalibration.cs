8ee596155cc33f3f6f0edb4bad42b11967b87a25
ziteh <honmonoh@gmail.com>
2021-04-01 17:23:43 +0800

    增加 ImageToWorld 的dataType轉換

--- Camera Calibration/CameraCalibration.cs
+++ Camera Calibration/CameraCalibration.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace CameraCalibration
 {
     public class CameraCalibration
     {
-        public PointF ImageToWorld()
+        public static PointF ImageToWorld(double[] imagePoint ,
+                                          double[,] intrinsicMatrix,
+                                          double[,] rotationMatrix,
+                                          double[] translationVector)
         {
+            var iP = Vector<double>.Build.DenseOfArray(imagePoint);
+            var iM = Matrix<double>.Build.DenseOfArray(intrinsicMatrix);
+            var rM = Matrix<double>.Build.DenseOfArray(rotationMatrix);
+            var tV = Vector<double>.Build.DenseOfArray(translationVector);
         }
     }
 }
