5c61995631b3620a808a3a9b169bf854fdb90c9d
ziteh <honmonoh@gmail.com>
2021-04-01 17:50:54 +0800

    增加 ImageToWorld 的矩陣運算

--- Camera Calibration/CameraCalibration.cs
+++ Camera Calibration/CameraCalibration.cs
@@ -15,10 +15,19 @@ namespace CameraCalibration
                                           double[,] rotationMatrix,
                                           double[] translationVector)
         {
-            var iP = Vector<double>.Build.DenseOfArray(imagePoint);
+            var iP = Matrix<double>.Build.DenseOfRowArrays(imagePoint);
             var iM = Matrix<double>.Build.DenseOfArray(intrinsicMatrix);
             var rM = Matrix<double>.Build.DenseOfArray(rotationMatrix);
             var tV = Vector<double>.Build.DenseOfArray(translationVector);
+
+            rM =  rM.SubMatrix(0, 2, 0, 3);
+            var tForm = rM.InsertRow(3,tV).Multiply(iM);
+            var result = iP.Multiply(tForm.Inverse());
+
+            PointF worldPoint = default;
+            worldPoint.X = (float)(result[0, 0] / result[2, 0]);
+            worldPoint.Y = (float)(result[1, 0] / result[2, 0]);
+            return worldPoint;
         }
     }
 }
