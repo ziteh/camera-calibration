89003214adea18e4f75858280a2a0ba086a50a87
ziteh <honmonoh@gmail.com>
2021-04-02 14:59:10 +0800

    Delete unused using

--- Camera Calibration/CameraCalibration.cs
+++ Camera Calibration/CameraCalibration.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Drawing.Drawing2D;
 using MathNet.Numerics.LinearAlgebra;
 
 namespace CameraCalibration
