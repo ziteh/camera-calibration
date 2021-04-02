08f8d25b4845c65e529941919b950150800a69e6
ziteh <honmonoh@gmail.com>
2021-04-02 12:36:07 +0800

    增加單元測試用的真實數據

--- CameraCalibration.UnitTests/CameraCalibrationUnitTests.cs
+++ CameraCalibration.UnitTests/CameraCalibrationUnitTests.cs
@@ -7,8 +7,29 @@ namespace CameraCalibration.UnitTests
     public class CameraCalibrationUnitTests
     {
         [Test]
-        public void TestMethod1()
+        public void ImageToWorld_InputRealData_OutputRealData()
+        { }
+
+
+        private static double[,] _intrinsicMatrix =
         {
-        }
+            { 1072.82150908417, 0, 0 },
+            { -2.67264859598147, 1072.75394896968, 0 },
+            { 665.983806358150, 398.397867569891, 1 }
+        };
+
+        private static double[,] _rotationMatrix =
+        {
+            { 0.9966, 0.0166, -0.0809 },
+            { -0.0248, 0.9946, -0.1011 },
+            { 0.0788, 0.1028, 0.9916 }
+        };
+
+        private static double[] _translationVector =
+        { -114.516132852586, -81.6117343486005, 385.812352338441 };
+
+        private static double[] _worldPoint = { 0, 125 ,0};
+
+        private static double[] _imagePoint = { 329.1558535, 521.2226187 };
     }
 }
